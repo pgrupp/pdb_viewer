3f87c77a702169bb17097dd1e0257f210f75031c
Patrick Grupp <patrickgrupp@me.com>
2017-01-21 00:14:19 +0100

    icon working

--- src/main/java/main/SimpleGraphView3DProgram.java
+++ src/main/java/main/SimpleGraphView3DProgram.java
@@ -1,11 +1,15 @@
 package main;
 
-import pdbmodel.PDBEntry;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pdbmodel.PDBEntry;
 import view.Presenter;
 import view.View;
+
+import javax.swing.ImageIcon;
+import java.awt.*;
+import java.net.URL;
 
 /**
  * Main class.
@@ -14,23 +18,30 @@ import view.View;
  */
 public class SimpleGraphView3DProgram extends Application {
 
-	@Override
-	public void start(Stage primaryStage) {
-		// Create the GUI view and a model graph.
-		View view = new View();
-		PDBEntry graph = new PDBEntry();
-		
-		// Set the scene and show it
-		Scene scene = new Scene(view);
-		primaryStage.setScene(scene);
+    @Override
+    public void start(Stage primaryStage) {
+        // Create the GUI view and a model graph.
+        View view = new View();
+        PDBEntry graph = new PDBEntry();
 
-		// The presenter handles connecting/updating view and model
-		new Presenter(view, graph, primaryStage);
 
-		primaryStage.setTitle("PDB Viewer");
-		//primaryStage.getIcons().add(new Image(SimpleGraphView3DProgram.class.getResourceAsStream("/images/pdb_viewer_icon.ico")));
-        //com.apple.eawt.Application.getApplication().setDockIconImage(new ImageIcon("src/main/resources/images/pdb_viewer_icon.ico").getImage());
-		primaryStage.show();
-	}
+        // Set the scene and show it
+        Scene scene = new Scene(view);
+        primaryStage.setScene(scene);
+
+        // The presenter handles connecting/updating view and model
+        new Presenter(view, graph, primaryStage);
+        primaryStage.setTitle("PDB Viewer");
+        javafx.scene.image.Image icon = new javafx.scene.image.Image(SimpleGraphView3DProgram.class.getResourceAsStream("/pdb_viewer.png"));
+        primaryStage.getIcons().add(icon);
+        try {
+            URL iconURL = SimpleGraphView3DProgram.class.getResource("/pdb_viewer.png");
+            Image ico = new ImageIcon(iconURL).getImage();
+            com.apple.eawt.Application.getApplication().setDockIconImage(ico);
+        } catch (Exception e) {
+            // Won't work on Windows or Linux.
+        }
+        primaryStage.show();
+    }
 
 }
