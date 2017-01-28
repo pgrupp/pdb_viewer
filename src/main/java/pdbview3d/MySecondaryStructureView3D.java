1a2d00a5d9761a8079d8549b7083530988cc47aa
Patrick Grupp <patrickgrupp@me.com>
2017-01-28 09:08:40 +0100

    cartoon view nearly done (meshes now working)

--- src/main/java/pdbview3d/MySecondaryStructureView3D.java
+++ src/main/java/pdbview3d/MySecondaryStructureView3D.java
@@ -48,10 +48,16 @@ class MySecondaryStructureView3D extends Group {
     }
 
     void compute() {
+        if (wasComputed) {
+            this.getChildren().clear();
+        }
         wasComputed = true;
         if (structure.getSecondaryStructureType().equals(SecondaryStructure.StructureType.betasheet)) {
             // structure is alpha helix
             TriangleMesh mesh = new TriangleMesh(VertexFormat.POINT_TEXCOORD);
+
+            PhongMaterial material = new PhongMaterial(Color.AQUAMARINE);
+            material.setSpecularColor(Color.AQUAMARINE.brighter());
 
             // set up the mesh arrays
 
@@ -79,13 +85,26 @@ class MySecondaryStructureView3D extends Group {
             Point3D direction = tarCA.subtract(sourCA).crossProduct(sourMirCB.subtract(sourCA)).normalize().multiply(sourCA.distance(sourMirCB)).multiply(BETA_SHEET_DEPTH);
             lastRef = "CB";
 
-            // We need to take care of the open hole in the sheet at the beginning (first residue between c beta, mirrored cbeta, shifted cbeta and shifted mirrored cbeta
-            //TODO
 
             // Set first source coordinates. This is important since otherwise for first element there is nothing to connect.
             Point3D initialBeta = getCBeta(first);
             Point3D initialMirrorBeta = getMirroredCBeta(first);
             setPoints(0, points, initialBeta, initialMirrorBeta, direction);
+
+            // We need to take care of the open end in the sheet at the beginning (first residue between c beta,
+            // mirrored cbeta, shifted cbeta and shifted mirrored cbeta
+            TriangleMesh endCap = new TriangleMesh(VertexFormat.POINT_TEXCOORD);
+            endCap.getTexCoords().addAll(0, 0);
+            endCap.getFaces().addAll(
+                    0, 0, 2, 0, 1, 0,
+                    1, 0, 2, 0, 3, 0
+            );
+            //Copy start points from other mesh
+            endCap.getPoints().addAll(points, 0, 12);
+            endCap.getFaceSmoothingGroups().addAll(1, 1);
+            MeshView endCapView = new MeshView(endCap);
+            endCapView.setMaterial(material);
+            this.getChildren().add(endCapView);
 
             for (int i = 1; i < listOfResidues.size(); i++) {
                 Residue residue = listOfResidues.get(i);
@@ -364,11 +383,14 @@ class MySecondaryStructureView3D extends Group {
             // Convert as mesh view in order to have a node to add to the scene graph
             MeshView meshView = new MeshView(mesh);
             meshView.setDrawMode(DrawMode.FILL);
+            meshView.setMaterial(material);
 
-            PhongMaterial mat = new PhongMaterial(Color.AQUAMARINE);
-            mat.setSpecularColor(Color.AQUAMARINE.brighter());
-            meshView.setMaterial(mat);
             this.getChildren().add(meshView);
+
+            createArrowHead(points, material,
+                    listOfResidues.get(listOfResidues.size() - 2),
+                    listOfResidues.get(listOfResidues.size() - 1)
+            );
 
         } else {
             // structure is alphahelix. Simple case.
@@ -386,6 +408,197 @@ class MySecondaryStructureView3D extends Group {
         }
     }
 
+    /**
+     * At the last residue of the beta sheet create a mesh structure which is pointing its direction in form of an
+     * arrowhead.
+     *
+     * @param points     The points array of the mesh structure. The last four points are taken from there in order to
+     *                   get the starting points for the arrow.
+     * @param material   The material which will be set to the mesh structure.
+     * @param secondLast The residue before last. Used to get the direction of the arrow.
+     * @param last       The last residue in the structure. Used to get the direction of the arrow (using the C alphas).
+     */
+    private void createArrowHead(float[] points, PhongMaterial material, Residue secondLast, Residue last) {
+        Point3D lastCAlpha = new Point3D(
+                secondLast.getCAlphaAtom().xCoordinateProperty().get(),
+                secondLast.getCAlphaAtom().yCoordinateProperty().get(),
+                secondLast.getCAlphaAtom().zCoordinateProperty().get()
+        );
+        Point3D cAlpha = new Point3D(
+                last.getCAlphaAtom().xCoordinateProperty().get(),
+                last.getCAlphaAtom().yCoordinateProperty().get(),
+                last.getCAlphaAtom().zCoordinateProperty().get()
+        );
+        Point3D direction = cAlpha.subtract(lastCAlpha).multiply(1.2); // calpha - lastCalpha ^= lastCalpha -> calpha
+
+        float[] newPoints = new float[10 * 3]; // 10 points a 3 coordinates
+        // Points 0:cbeta, 1:cbeta mirrored, 2: cbeta shifted, 3:cbeta mirrored shifted
+        System.arraycopy(points, points.length - 12, newPoints, 0, 12);
+        Point3D cBeta = new Point3D(newPoints[0], newPoints[1], newPoints[2]);
+        Point3D cBetaMir = new Point3D(newPoints[3], newPoints[4], newPoints[5]);
+        Point3D cBetaShif = new Point3D(newPoints[6], newPoints[7], newPoints[8]);
+        Point3D cBetaShifMir = new Point3D(newPoints[9], newPoints[10], newPoints[11]);
+        Point3D cAlphaShifted = cBetaShif.midpoint(cBetaShifMir);
+
+        Point3D outerDirection = cBeta.subtract(cAlpha);
+
+
+        Point3D outerPoint = cBeta.add(outerDirection);
+        Point3D outerPointShifted = cBetaShif.add(outerDirection);
+
+        Point3D outerMirroredPoint = cBetaMir.add(outerDirection.multiply(-1));
+        Point3D outerMirroredPointShifted = cBetaShifMir.add(outerDirection.multiply(-1));
+
+        Point3D arrowTip = cAlpha.add(direction);
+        Point3D arrowTipShifted = cAlphaShifted.add(direction);
+
+        //  arrow side, Point 4
+        newPoints[12] = (float) outerPoint.getX();
+        newPoints[13] = (float) outerPoint.getY();
+        newPoints[14] = (float) outerPoint.getZ();
+        // arrow side shifted, point 5
+        newPoints[15] = (float) outerPointShifted.getX();
+        newPoints[16] = (float) outerPointShifted.getY();
+        newPoints[17] = (float) outerPointShifted.getZ();
+        //arrow mirrored side, point 6
+        newPoints[18] = (float) outerMirroredPoint.getX();
+        newPoints[19] = (float) outerMirroredPoint.getY();
+        newPoints[20] = (float) outerMirroredPoint.getZ();
+        // arrow mirrored side shifted, point 7
+        newPoints[21] = (float) outerMirroredPointShifted.getX();
+        newPoints[22] = (float) outerMirroredPointShifted.getY();
+        newPoints[23] = (float) outerMirroredPointShifted.getZ();
+
+        //arrow tip, point 8
+        newPoints[24] = (float) arrowTip.getX();
+        newPoints[25] = (float) arrowTip.getY();
+        newPoints[26] = (float) arrowTip.getZ();
+        // arrow tip shifted, point 9
+        newPoints[27] = (float) arrowTipShifted.getX();
+        newPoints[28] = (float) arrowTipShifted.getY();
+        newPoints[29] = (float) arrowTipShifted.getZ();
+
+        // Define the faces
+        int[] faces = new int[10 * 6]; // Ten faces: 4 for  tip sides, four for each outer and two for upper and lower
+        if(lastRef.equals("CBM")) {
+            // to arrow side
+            faces[0] = 0;
+            faces[2] = 2;
+            faces[4] = 5;
+
+            faces[6] = 0;
+            faces[8] = 5;
+            faces[10] = 4;
+
+            //to mirrored arrowside
+            faces[12] = 1;
+            faces[14] = 6;
+            faces[16] = 3;
+
+            faces[18] = 3;
+            faces[20] = 6;
+            faces[22] = 7;
+
+            // to arrow tip
+            faces[24] = 4;
+            faces[26] = 5;
+            faces[28] = 8;
+
+            faces[30] = 5;
+            faces[32] = 9;
+            faces[34] = 8;
+
+            //to arrow tip mirrored
+
+            faces[36] = 6;
+            faces[38] = 9;
+            faces[40] = 7;
+
+            faces[42] = 9;
+            faces[44] = 6;
+            faces[46] = 8;
+
+            //Arrow top (shifted)
+            faces[48] = 5;
+            faces[50] = 7;
+            faces[52] = 9;
+
+            //Arrow lower
+            faces[54] = 6;
+            faces[56] = 4;
+            faces[58] = 8;
+        } else {
+            // Last ref was CBeta therfore take the same faces as above, but tunr them (take the other side, by
+            // flipping the las two numbers in each face)
+            // to arrow side
+            faces[0] = 0;
+            faces[2] = 5;
+            faces[4] = 2;
+
+            faces[6] = 0;
+            faces[8] = 4;
+            faces[10] = 5;
+
+            //to mirrored arrowside
+            faces[12] = 1;
+            faces[14] = 3;
+            faces[16] = 6;
+
+            faces[18] = 3;
+            faces[20] = 7;
+            faces[22] = 6;
+
+            // to arrow tip
+            faces[24] = 4;
+            faces[26] = 8;
+            faces[28] = 5;
+
+            faces[30] = 5;
+            faces[32] = 8;
+            faces[34] = 9;
+
+            //to arrow tip mirrored
+
+            faces[36] = 6;
+            faces[38] = 7;
+            faces[40] = 9;
+
+            faces[42] = 9;
+            faces[44] = 8;
+            faces[46] = 6;
+
+            //Arrow top (shifted)
+            faces[48] = 5;
+            faces[50] = 9;
+            faces[52] = 7;
+
+            //Arrow lower
+            faces[54] = 6;
+            faces[56] = 8;
+            faces[58] = 4;
+        }
+
+        TriangleMesh arrowHead = new TriangleMesh(VertexFormat.POINT_TEXCOORD);
+        arrowHead.getPoints().addAll(newPoints);
+        arrowHead.getFaces().addAll(faces);
+        arrowHead.getTexCoords().addAll(0,0);
+        arrowHead.getFaceSmoothingGroups().addAll(0,0,0,0,0,0,0,0,0,0);
+        MeshView arrowHeadMeshView = new MeshView(arrowHead);
+        arrowHeadMeshView.setMaterial(material);
+        this.getChildren().add(arrowHeadMeshView);
+    }
+
+    /**
+     * Set points in the points array for the triangle mesh.
+     *
+     * @param idx            The index in the points array where to put the points.
+     * @param points         The points array which will be used to store the points in. Will be in the form for the
+     *                       triangle mesh structure.
+     * @param cBeta          The c Beta point to put in.
+     * @param mirroredCBeta  The mirrored point of C beta (should be mirrored at the residue's c alpha.
+     * @param directionShift The shift direction (only the direction, not the location vector) with appropriate length
+     *                       where to put the shifted c beta and shifted mirrored c beta.
+     */
     private void setPoints(int idx, float[] points, Point3D cBeta, Point3D mirroredCBeta, Point3D directionShift) {
         // Set C Beta
         points[idx] = (float) cBeta.getX();
@@ -408,8 +621,9 @@ class MySecondaryStructureView3D extends Group {
     /**
      * Get the direction (with appropriate length) to shift the target's points (C alpha, C beta and mirrored C beta)
      * in 3D space in order to create points to reference to in order to get a beta sheet with a depth.
-     * @param source the last residue
-     * @param target The residue to get the shift for.
+     *
+     * @param source   the last residue
+     * @param target   The residue to get the shift for.
      * @param crossing Are the two C Betas of the two residues on 'the same side of the backbone' or crossing. Crossing
      *                 means that Cbeta of the source is connected to mirrored C Beta of the target and vice versa for
      *                 mirrored CBeta of the source.
@@ -428,8 +642,8 @@ class MySecondaryStructureView3D extends Group {
         Point3D dest = sourceAlpha.subtract(targetAlpha);
         Point3D ref;
 
-            // Depending on the last reference point (either C beta or CBeta mirrored) we need to use the correct one
-            // in order to get the shift right and not crossing.
+        // Depending on the last reference point (either C beta or CBeta mirrored) we need to use the correct one
+        // in order to get the shift right and not crossing.
         if (crossing) {
             if (lastRef.equals("CB")) {
                 // Last ref was CBeta so we need to take the mirrored target CB point since we are 'crossing'
@@ -455,6 +669,7 @@ class MySecondaryStructureView3D extends Group {
 
     /**
      * Get the mirrored point of the given residue's c beta.
+     *
      * @param residue Reference residue
      * @return Point which is C beta mirrored at C alpha.
      */
@@ -466,6 +681,7 @@ class MySecondaryStructureView3D extends Group {
 
     /**
      * Get a points C alpha in 3D space.
+     *
      * @param residue The residue to get C alpha from.
      * @return Position in 3D space of C alpha of the given residue.
      */
@@ -477,6 +693,7 @@ class MySecondaryStructureView3D extends Group {
 
     /**
      * Get the point in 3D space of C Beta of the given residue.
+     *
      * @param residue The residue to get C beta from.
      * @return Position in 3D space of C beta of the given residue.
      */
